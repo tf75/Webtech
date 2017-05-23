b65adf7e1f7ee5793d93653090c3f4a5de143a22
tf75 <tf16843@bristol.ac.uk>
2017-05-23 19:51:54 +0100

    Add files via upload

---
+++ funcs.java
@@ -0,0 +1,24 @@
+/*var div = document.getElementById("myDIV");
+div.onmouseover = function(){
+	div.style.opacity = 0;
+	window.alert("HELLO");
+	window.open(div.href);
+}
+function mouseOver() {
+    document.getElementById("demo").style.opacity = 0;
+    window.alert("HELLO");
+}  
+
+function mouseOut() {
+    document.getElementById("demo1").style.opacity: 0;
+ }
+*/
+
+function mouseOver() {
+    document.getElementById("demo").style.opacity = 0;
+    window.alert("HELLO");
+}
+
+function mouseOut() {
+    document.getElementById("demo1").style.opacity: 0;
+ }
---
+++ funcs.js
@@ -0,0 +1,70 @@
+
+function mouse1overbox(){
+
+	var on = document.getElementById("1demo").style.opacity = 0.75;
+}
+
+function mouse1notbox(){
+
+	var on = document.getElementById("1demo").style.opacity = 1;
+}
+
+function mouse2overbox(){
+
+	var on = document.getElementById("2demo").style.opacity = 0.75;
+}
+
+function mouse2notbox(){
+
+	var on = document.getElementById("2demo").style.opacity = 1;
+}
+
+function mouse3overbox(){
+
+	var on = document.getElementById("3demo").style.opacity = 0.75;
+}
+
+function mouse3notbox(){
+
+	var on = document.getElementById("3demo").style.opacity = 1;
+}
+
+function mouse4overbox(){
+
+	var on = document.getElementById("4demo").style.opacity = 0.75;
+}
+
+function mouse4notbox(){
+
+	var on = document.getElementById("4demo").style.opacity = 1;
+}
+
+function mouse5overbox(){
+
+	var on = document.getElementById("5demo").style.opacity = 0.75;
+}
+
+function mouse5notbox(){
+
+	var on = document.getElementById("5demo").style.opacity = 1;
+}
+
+function mouse6overbox(){
+
+	var on = document.getElementById("6demo").style.opacity = 0.75;
+}
+
+function mouse6notbox(){
+
+	var on = document.getElementById("6demo").style.opacity = 1;
+}
+
+function mouse7overbox(){
+
+	var on = document.getElementById("7demo").style.opacity = 0.75;
+}
+
+function mouse7notbox(){
+
+	var on = document.getElementById("7demo").style.opacity = 1;
+}
